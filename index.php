cb909d85912ef8aa6c3f85a241419302af938909
Collin Reisdorf <collin@paperclipped.com>
2011-08-16 10:57:09 -0400

    adding phpinfo() page to test server

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+// mysql_password           : urJmMwpfwHqUddT
+// mysql_dbsize             : 5
+// mysql_database           : depdv5xbcq5
+// mysql_hostname           : 127.0.0.1
+// mysql_username           : depdv5xbcq5
+
+phpinfo();
+
+?>
